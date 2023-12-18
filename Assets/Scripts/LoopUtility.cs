84287ebaa0e422cc9abc8a8781379cabd18a27a7
Illyve <43428399+Illyve@users.noreply.github.com>
2023-12-18 01:50:19 -0600

    Added title screen music

--- Assets/Scripts/LoopUtility.cs
+++ Assets/Scripts/LoopUtility.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class LoopUtility : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip intro;
+    [SerializeField]
+    private AudioClip loop;
+    [SerializeField]
+    private AudioSource audioSource;
 
-    [SerializeField]
-    private AudioSource intro;
-    [SerializeField]
-    private AudioSource loop;
     // Start is called before the first frame update
     void Start()
     {
-        intro.PlayScheduled(AudioSettings.dspTime + 0.25f);
-        loop.PlayScheduled(AudioSettings.dspTime + 0.17f + intro.clip.length);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (audioSource.timeSamples > intro.samples + loop.samples)
+        {
+            audioSource.timeSamples -= loop.samples;
+        }
     }
 }
